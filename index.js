63848c65aa2e0fdd73f6dd920d740bf4df1554cf
its-me-nishmal <twalaba123@gmail.com>
2024-02-14 22:38:16 +0530

    Test commit

--- index.js
+++ index.js
@@ -6,20 +6,24 @@ function lazyGit(message = 'Initial commit', options = {}) {
   const { b: branch = 'main', r: remote = 'origin' } = options;
   const git = simpleGit();
 
+  // Define a callback function for the push operation
+  const pushCallback = (err) => {
+    if (err) {
+      if (isFirstPush) {
+        console.error('Please push manually using "git push" command for the first time.');
+        isFirstPush = false;
+      } else {
+        console.error('Error:', err);
+      }
+    } else {
+      console.log('Changes committed and pushed successfully!');
+    }
+  };
+
+  // Add, commit, and push changes
   git.add('.')
     .commit(message)
-    .push(remote, branch, (err) => {
-      if (err) {
-        if (isFirstPush) {
-          console.error('Please push manually using "git push" command for the first time.');
-          isFirstPush = false;
-        } else {
-          console.error('Error:', err);
-        }
-      } else {
-        console.log('Changes committed and pushed successfully!');
-      }
-    });
+    .push(remote, branch, pushCallback);
 }
 
 module.exports = lazyGit;
